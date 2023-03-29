451b5ab71bd563f24f77f055813b2a380e6f4b6f
tkbessler <tkbessler@gmail.com>
2023-03-29 08:42:25 -0500

    property fix

--- constants.js
+++ constants.js
@@ -39,36 +39,39 @@ const interpolate = {
     default: true
 }
 
-const commandDataTypes = {
+const constraintProps = {
     frameProps: {default: 0, min: 0, max: 39, type: "Integer"},
     secondProps: {default: 0, min: 0, max: 59, type: "Integer"},
     minuteProps: {default: 0, min: 0, max: 99, type: "Integer"},
+    zoomProps: {default: 2, min: -50, max: 50, type: "Float"},
+    xProps: {default: 0, min: -100, max: 100, type: "Float"},
+    yProps: {default: 0, min: -100, max: 100, type: "Float"},
+    wProps: {default: 0.4, min: 0, max: 2, type: "Float"},
+    hProps: {default: 0.4, min: 0, max: 2, type: "Float"},
+    focusProps: {default: 0, min: 0, max: 1, type: "Float"},
+    timeProps: {default: 1, min: 0.01, max: 10, type: "Float"}
+}
+
+const commandDataTypes = {
     Zoom: {
         name: "Zoom",
         template: [[0,0,0], 2],
-        header: "getAutoZoom=createZoomer({0},{1});",
-        zoomProps: {default: 2, min: -50, max: 50, type: "Float"}
+        header: "getAutoZoom=createZoomer({0},{1});"
     },
     CameraPan: {
         name: "Camera Pan",
         template: [[0,0,0], {w: 0.4, h: 0.4, x: 0, y: 0}],
-        header: "getCamBounds=createBoundsPanner({0},{1});",
-        xProps: {default: 0, min: -100, max: 100, type: "Float"},
-        yProps: {default: 0, min: -100, max: 100, type: "Float"},
-        wProps: {default: 0.4, min: 0, max: 2, type: "Float"},
-        hProps: {default: 0.4, min: 0, max: 2, type: "Float"}
+        header: "getCamBounds=createBoundsPanner({0},{1});"
     },
     CameraFocus: {
         name: "Camera Focus",
         template: [[0,0,0], [1]],
-        header: "getCamFocus=createFocuser({0},{1});",
-        focusProps: {default: 0, min: 0, max: 1, type: "Float"}
+        header: "getCamFocus=createFocuser({0},{1});"
     },
     TimeRemap: {
         name: "Time Remap",
         template: [[0,0,0], 1],
-        header: "timeRemapper=createTimeRemapper({0},{1});",
-        timeProps: {default: 1, min: 0.01, max: 10, type: "Float"}
+        header: "timeRemapper=createTimeRemapper({0},{1});"
     }
 }
 
