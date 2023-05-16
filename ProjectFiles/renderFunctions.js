efab9c82edb9734bfbbff93d74a8501d78546420
Malizma <malizma333@gmail.com>
2023-05-16 08:44:10 -0500

    flag image with test interaction

--- ProjectFiles/renderFunctions.js
+++ ProjectFiles/renderFunctions.js
@@ -400,9 +400,36 @@ function skinEditorComp (create, root, data) {
     create('div', {
       style: {
         ...triggerWindowStyle,
-        overflow: 'hidden'
+        overflow: 'hidden',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
       }
-    }
+    }, FlagComponent(create, root)
     )
   )
+}
+
+function FlagComponent (create, root) {
+  return create('svg', {
+    transform: 'scale(5)',
+    width: '15',
+    height: '18'
+  },
+  create('path', {
+    transform: 'translate(-5, -3)',
+    fill: 'rgba(0,0,0,0.4)',
+    d: 'M6,3A1,1 0 0,1 7,4V4.88C8.06,4.44 9.5,4 11,4C14,4 14,6 16,6C19,6 20,4 20,4V12C20,12 19,14 16,14C13,14 13,12 11,12C8,12 7,14 7,14V21H5V4A1,1 0 0,1 6,3Z'
+  }),
+  create('path', {
+    transform: 'translate(-5, -3)',
+    fill: 'rgba(0,0,0,0.4)',
+    d: 'M6,3A1,1 0 0,1 7,4V4.88C8.06,4.44 9.5,4 11,4C14,4 14,6 16,6C19,6 20,4 20,4V12C20,12 19,14 16,14C13,14 13,12 11,12C8,12 7,14 7,14V21H5V4A1,1 0 0,1 6,3M7,7.25V11.5C7,11.5 9,10 11,10C13,10 14,12 16,12C18,12 18,11 18,11V7.5C18,7.5 17,8 16,8C14,8 13,6 11,6C9,6 7,7.25 7,7.25Z',
+    onClick: () => doSomething()
+  })
+  )
+}
+
+function doSomething () {
+  console.log('Hi')
 }
