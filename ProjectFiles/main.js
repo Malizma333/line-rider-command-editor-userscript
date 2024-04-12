a6e9aff36ed5085a9912619b1e421d8ed5ba8ae5
tkbessler <tkbessler@gmail.com>
2024-04-12 15:14:17 -0500

    backwards compatible file

---
+++ ProjectFiles/main.js
@@ -0,0 +1,45 @@
+// Main function, entry point of the application
+
+function main() {
+  window.V2 = window.V2 || Selectors.getWindowStart(window.store.getState());
+
+  const { React, ReactDOM } = window;
+
+  const parent = document.createElement('div');
+  document.getElementById('content').appendChild(parent);
+
+  class LegacyComponent extends React.Component {
+    constructor() {
+      super();
+
+      this.state = {};
+    }
+
+    componentDidMount() {
+      Object.assign(parent.style, {
+        border: '2px solid black',
+        left: '50px',
+        position: 'fixed',
+        top: '12.5px',
+      });
+    }
+
+    render() {
+      return React.createElement(
+        'div',
+        null,
+        'Outdated command editor userscript detected! Please update by following the link below.',
+        React.createElement('br'),
+        React.createElement('a', { href: 'https://github.com/Malizma333/linerider-userscript-mods/raw/master/mods/line-rider-command-editor.user.js' }, 'Click Here'),
+      );
+    }
+  }
+
+  ReactDOM.render(React.createElement(LegacyComponent), parent);
+}
+
+if (window.store) {
+  main();
+} else {
+  window.onAppReady = main;
+}
